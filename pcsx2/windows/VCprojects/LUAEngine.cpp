5f709b7133b4f09f81b2974070322e5714f680fc
TopazTK <>
2020-10-21 00:18:00 +0300

    Update LUAEngine [Untested Functionality, breaks compatibility. ]

--- pcsx2/windows/VCprojects/LUAEngine.cpp
+++ pcsx2/windows/VCprojects/LUAEngine.cpp
@@ -3,11 +3,21 @@
 #include "Memory.h"
 
 #include "sol.hpp"
+#include <iostream>
+#include <fstream>
 
-sol::state LUAEngine;
+using namespace sol;
+using namespace std;
 
+state LUAEngine;
 sol::function ExecuteOnInit;
 sol::function ExecuteOnFrame;
+
+wxString _rootPath;
+wxString _loadPath;
+wxString _dumpPath;
+
+int _luaVersion = 0x03;
 
 namespace
 {
@@ -27,7 +37,7 @@ namespace
 	{
 		return (float)memRead32(Input);
 	}
-	std::vector<u8> Read05(u32 Input01, u32 Input02)
+	vector<u8> Read05(u32 Input01, u32 Input02)
 	{
 		std::vector<u8> Value(Input02);
 
@@ -44,6 +54,48 @@ namespace
 	{
 		u32 Value = memRead32(Input01);
 		return Value + Input02;
+	}
+
+	void File01(u32 Input01, u32 Input02, const char* Input03)
+	{
+		std::vector<u8> _value(Input02);
+		wxString _location = Path::Combine(_dumpPath, Input03);
+
+		for (size_t i = 0; i < Input02; i++)
+		{
+			_value.at(i) = memRead8(Input01);
+			Input01++;
+		}
+
+		ofstream _output(_location.ToStdString(), ios::out | ios::binary);
+
+		if (!_output)
+		{
+			Console.WriteLn(Color_Red, "Unable to open file: " + wxString(Input03));
+			return;
+		}
+
+		_output.write(reinterpret_cast<char*>(&_value[0]), _value.size() * sizeof(u8));
+	}
+	void File02(u32 Input01, const char* Input02)
+	{
+		wxString _location = Path::Combine(_dumpPath, Input02);
+
+		ifstream _input(_location.ToStdString(), ios_base::binary);
+
+		_input.seekg(0, std::ios_base::end);
+		size_t _length = _input.tellg();
+		_input.seekg(0, std::ios_base::beg);
+
+		vector<char> _buffer;
+		_buffer.reserve(_length);
+		copy(istreambuf_iterator<char>(_input), istreambuf_iterator<char>(), back_inserter(_buffer));
+
+		for (size_t i = 0; i < _buffer.size(); i++)
+		{
+			memWrite8(Input01, (u8)_buffer[i]);
+			Input01++;
+		}
 	}
 
 	void Write01(u32 Input01, u8 Input02)
@@ -66,32 +118,45 @@ namespace
 		if (memRead32(Input01) != (u32)Input02)
 			memWrite32(Input01, (u32)Input02);
 	}
-
+	void Write05(u32 Input01, vector<u8> Input02)
+	{
+		for (size_t i = 0; i < Input02.size(); i++)
+		{
+			memWrite8(Input01, (u8)Input02[i]);
+			Input01++;
+		}
+	}
+	
 	void Misc01(const char* Input, int Color = 1)
 	{
 		ConsoleColors _color = (ConsoleColors)Color;
 		Console.WriteLn(_color, Input);
 	}
-} // namespace
+}
 
 
 void ExportFunctionCalls()
 {
 	// Readers
 	LUAEngine.set_function("ReadByte", Read01);
-	LUAEngine.set_function("ReadUShort", Read02);
-	LUAEngine.set_function("ReadUInt", Read03);
+	LUAEngine.set_function("ReadShort", Read02);
+	LUAEngine.set_function("ReadInt", Read03);
 	LUAEngine.set_function("ReadFloat", Read04);
 	LUAEngine.set_function("ReadArray", Read05);
 
 	// Calculators
 	LUAEngine.set_function("GetPointer", Calc01);
 
+	// IO Operations
+	LUAEngine.set_function("CreateDump", File01);
+	LUAEngine.set_function("WriteFile", File02);
+
 	// Writers
 	LUAEngine.set_function("WriteByte", Write01);
-	LUAEngine.set_function("WriteUShort", Write02);
-	LUAEngine.set_function("WriteUInt", Write03);
+	LUAEngine.set_function("WriteShort", Write02);
+	LUAEngine.set_function("WriteInt", Write03);
 	LUAEngine.set_function("WriteFloat", Write04);
+	LUAEngine.set_function("WriteArray", Write05);
 
 	// Misc
 	LUAEngine.set_function("Print", Misc01);
@@ -103,14 +168,14 @@ bool InitScript(wxString path, wxString ScriptTitle)
 
 	LUAEngine.open_libraries
 	(
-		sol::lib::base,		 
-		sol::lib::package,
-		sol::lib::coroutine,
-		sol::lib::string,
-		sol::lib::os,
-		sol::lib::math,
-		sol::lib::table,
-		sol::lib::io
+		lib::base,		 
+		lib::package,
+		lib::coroutine,
+		lib::string,
+		lib::os,
+		lib::math,
+		lib::table,
+		lib::io
 	);
 
 	ExportFunctionCalls();
@@ -125,6 +190,12 @@ bool InitScript(wxString path, wxString ScriptTitle)
 		Console.WriteLn(Color_Black, L"");
 		return false;
 	}
+
+	_loadPath = Path::Combine(Path::GetDirectory(path),  "io_load");
+	_dumpPath = Path::Combine(Path::GetDirectory(path), "io_dump");
+
+	Console.WriteLn(Color_Black, _loadPath);
+	Console.WriteLn(Color_Black, _dumpPath);
 
 	Console.WriteLn(Color_Green, L"--> Initialization Successful!");
 	Console.WriteLn(Color_Black, L"");
